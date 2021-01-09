938b63dc379f2354491909a040747f5f348500f6
Shane Robertson <sjrobertson0812@gmail.com>
2021-01-08 21:48:34 -0500

    added shaneBranch1 and index.js file

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+// { useState, useEffect }
+// import {
+//   getSomething
+// } from '../api';
+
+const App = () => {
+//   const [message, setMessage] = useState('');
+
+//   useEffect(() => {
+//     getSomething()
+//       .then(response => {
+//         setMessage(response.message);
+//       })
+//       .catch(error => {
+//         setMessage(error.message);
+//       });
+//   });
+
+  return (
+    <div className="App">
+      <h1>Hello, World!</h1>
+
+    </div>
+  );
+}
+
+export default App;
